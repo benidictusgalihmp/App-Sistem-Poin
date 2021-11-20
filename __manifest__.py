4f3ffdba137bd9c583b9aa092e32c506bbee6e78
Farhan <ahandnr@gmail.com>
2021-11-20 17:57:56 +0700

    bikin security

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/menus.xml',
     ],
     'demo': [
