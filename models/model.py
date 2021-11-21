20874c06e28bcc867929264c3ec6788cd21d6753
Jesica <13519011@std.stei.itb.ac.id>
2021-11-21 11:36:22 +0700

    update riwayat

--- models/model.py
+++ models/model.py
@@ -25,41 +25,59 @@ class Anggota(models.Model):
 
     # tambah
     def tambah100(self):
+        poinl = self.poin
         self.poin = self.poin + 100
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
     def tambah200(self):
+        poinl = self.poin
         self.poin = self.poin + 200
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
     def tambah500(self):
+        poinl = self.poin
         self.poin = self.poin + 500
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
     def tambah1000(self):
+        poinl = self.poin
         self.poin = self.poin + 1000
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
 
     # redeem
     def redeem100(self):
         if self.poin < 100:
             raise ValidationError(("Poin tidak cukup."))
+        poinl = self.poin
         self.poin = self.poin - 100
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
+
     def redeem200(self):
         if self.poin < 200:
             raise ValidationError(("Poin tidak cukup."))
+        poinl = self.poin
         self.poin = self.poin - 200
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
+
+    def redeem500(self):
         if self.poin < 500:
             raise ValidationError(("Poin tidak cukup."))
-    def redeem500(self):
+        poinl = self.poin
         self.poin = self.poin - 500
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
+
     def redeem1000(self):
         if self.poin < 1000:
             raise ValidationError(("Poin tidak cukup."))
+        poinl = self.poin
         self.poin = self.poin - 1000
         self.write({'poin' : str(self.poin)})
+        self.tambahRiwayat(poinl)
     
-
 
     @api.constrains('id_anggota')
     def check_id_anggota(self):
@@ -67,9 +85,37 @@ class Anggota(models.Model):
             anggota = self.env['anggota.anggota'].search([('id_anggota','=',rec.id_anggota),('id','!=',rec.id)])
             if anggota:
                 raise ValidationError(("ID %s Already Exists" % rec.id_anggota))
-            
 
+    def tambahRiwayat(self, poin_lama):
+        perubahan_poin = self.poin - poin_lama
+
+        transaksi = ''
+        if(perubahan_poin>0):
+            # tambah poin
+            transaksi = transaksi + '+' + str(perubahan_poin)
+        else:
+            # redeem poin
+            transaksi = str(perubahan_poin)
+
+        vals = {
+            'id_anggota' : self.id_anggota,
+            'nama' : self.name,
+            'poin_lama' : poin_lama,
+            'poin_baru' : self.poin,
+            'transaksi' : perubahan_poin
+        }
+        self.env['riwayat.riwayat'].create(vals)
+
+        print("TAMBAH POIN IS CLICKED")
+        print(vals)
 
 class Riwayat(models.Model):
     _name = 'riwayat.riwayat' # membuat tabel riwayat pada postgres 
     _description = 'Data riwayat' # deskripsi tabel
+
+    id_anggota = fields.Integer(string="ID", required=True)
+    nama = fields.Char(string="Nama", required=True)
+    poin_lama = fields.Integer(string="Poin Lama", required=True)
+    poin_baru = fields.Integer(string="Poin Baru", required=True)
+    transaksi = fields.Char(string="Transaksi", required=True)
+    write_date = fields.Datetime(string='Waktu')
